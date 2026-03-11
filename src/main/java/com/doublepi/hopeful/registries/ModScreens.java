482cdb43c239025be464ed68d7d59db600ad6fb2
Tau's PC <yarinhalali2015@gmail.com>
2026-03-11 19:28:09 +0200

    v1.1 - actually MVP

--- src/main/java/com/doublepi/hopeful/registries/ModScreens.java
+++ src/main/java/com/doublepi/hopeful/registries/ModScreens.java
@@ -1,12 +1,12 @@
 package com.doublepi.hopeful.registries;
 
-import com.doublepi.hopeful.content.anvil.AnvilScreen;
+
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
 public class ModScreens {
 
     public static void register(RegisterMenuScreensEvent event){
-        event.register(ModMenus.NEW_ANVIL_MENU.get(), AnvilScreen::new);
+     //   event.register(ModMenus.NEW_ANVIL_MENU.get(), AnvilScreen::new);
     }
 }
 
