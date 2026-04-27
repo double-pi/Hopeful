30c4f790a3bbe26689238702594b2c9872d10997
Tau's PC <yarinhalali2015@gmail.com>
2026-04-27 21:30:15 +0300

    Start of default packs

---
+++ src/main/java/com/doublepi/hopeful/registries/ModOptionalPacks.java
@@ -0,0 +1,44 @@
+package com.doublepi.hopeful.registries;
+
+import com.doublepi.hopeful.HopefulMod;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+import net.neoforged.neoforge.resource.ResourcePackLoader;
+import org.jspecify.annotations.NonNull;
+
+public class ModOptionalPacks {
+
+
+    @SubscribeEvent
+    public static void addPackFinders(AddPackFindersEvent event){
+        if(event.getPackType() == PackType.SERVER_DATA){
+            event.addPackFinders(
+                    Identifier.fromNamespaceAndPath(HopefulMod.MODID,"datapacks/hopeful_unlimited"),
+                    PackType.SERVER_DATA,
+                    Component.translatable("dataPack.hopeful.hopeful_unlimited.name"),
+                    createSource(false), false, Pack.Position.TOP);
+//            event.addPackFinders(id("resourcepacks/villager_trades"), PackType.SERVER_DATA,
+//                    Component.translatable("dataPack.enchiridion.villager_trades.name"),
+//                    createSource(true), false, Pack.Position.TOP);
+        }
+    }
+
+    public static PackSource createSource(boolean enabledByDefault) {
+        return new PackSource() {
+            @Override
+            public @NonNull Component decorate(@NonNull Component component) {
+                return Component.translatable("pack.hopeful.builtin", component);
+            }
+
+            @Override
+            public boolean shouldAddAutomatically() {
+                return enabledByDefault;
+            }
+        };
+    }
+}
